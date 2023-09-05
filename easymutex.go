6c75fa0cee25f75ddbe3c7dae782369072c9938f
Jille Timmermans <jille@quis.cx>
2023-09-05 22:22:09 +0200

    Add convenience functions like LockMutex that create+lock
    So it needs fewer lines to be used

--- easymutex.go
+++ easymutex.go
@@ -5,6 +5,12 @@ package easymutex
 
 import "sync"
 
+// LockLocker wraps a sync.Locker in an EasyLocker and locks it.
+func LockLocker(l sync.Locker) *EasyLocker {
+	l.Lock()
+	return &EasyLocker{L: l, Held: true}
+}
+
 // EasyLocker is a wrapper around sync.Locker that keeps track of whether we have the mutex.
 type EasyLocker struct {
 	L    sync.Locker
@@ -23,6 +29,12 @@ func (e *EasyLocker) Unlock() {
 		e.L.Unlock()
 		e.Held = false
 	}
+}
+
+// LockMutex wraps a *sync.Mutex in an EasyMutex and locks it.
+func LockMutex(l *sync.Mutex) *EasyMutex {
+	l.Lock()
+	return &EasyMutex{L: l, Held: true}
 }
 
 // EasyMutex is a wrapper around *sync.Mutex that keeps track of whether we have the mutex.
@@ -51,6 +63,18 @@ func (e *EasyMutex) Unlock() {
 		e.L.Unlock()
 		e.Held = false
 	}
+}
+
+// LockRWMutex wraps a *sync.RWMutex in an EasyRWMutex and locks it exclusively.
+func LockRWMutex(l *sync.RWMutex) *EasyRWMutex {
+	l.Lock()
+	return &EasyRWMutex{L: l, HeldExclusive: true}
+}
+
+// RLockRWMutex wraps a *sync.RWMutex in an EasyRWMutex and locks it in shared mode.
+func RLockRWMutex(l *sync.RWMutex) *EasyRWMutex {
+	l.RLock()
+	return &EasyRWMutex{L: l, HeldShared: true}
 }
 
 // EasyRWMutex is a wrapper around *sync.RWMutex that keeps track of whether we have the mutex.
